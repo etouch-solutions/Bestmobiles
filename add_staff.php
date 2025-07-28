330f8849e38c9c65c3d2cfd34b5e88fdba160b5c
etouch-solutions <etouchindiasolutions@gmail.com>
2025-07-28 14:29:33 +0530

    Add full project files

--- add_staff.php
+++ add_staff.php
@@ -3,7 +3,19 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
 
+<?php
+$host = 'localhost';  
+$user = 'u520351775_etouch';
+$pass = '!@#Admin@4321';  
+$db   = 'u520351775_bestmobiles';  
+$conn = new mysqli($host, $user, $pass, $db);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
+ 
 
 <?php
 // Connect DB
