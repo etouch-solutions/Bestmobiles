90b5b7f566cd3f410665b2da7139d4906440591d
etouch-solutions <etouchindiasolutions@gmail.com>
2025-07-30 13:30:11 +0530

    Add full project files

--- claim_entry.php
+++ claim_entry.php
@@ -1,18 +1,22 @@
 <?php
- 
 include 'db.php';
 header('Content-Type: application/json');
 
 $q = mysqli_real_escape_string($conn, $_GET['q'] ?? '');
 
 $data = [];
+
 if ($q !== '') {
     $sql = "SELECT i.Insurance_Entry_Id, c.Cus_Name, i.Product_Model_Name, i.IMEI_1 
             FROM Insurance_Entry i
             JOIN Customer_Master c ON c.Cus_Id = i.Cus_Id
-            WHERE c.Cus_Name LIKE '%$q%' OR c.Cus_CNo LIKE '%$q%' OR i.IMEI_1 LIKE '%$q%'
+            WHERE c.Cus_Name LIKE '%$q%' 
+               OR c.Cus_CNo LIKE '%$q%' 
+               OR i.IMEI_1 LIKE '%$q%'
             LIMIT 10";
+
     $result = mysqli_query($conn, $sql);
+
     while ($row = mysqli_fetch_assoc($result)) {
         $data[] = [
             'insurance_entry_id' => $row['Insurance_Entry_Id'],
@@ -22,9 +26,11 @@ if ($q !== '') {
         ];
     }
 }
+
 echo json_encode($data);
 ?>
-<?php include 'db.php'; ?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
