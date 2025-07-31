22ffcf72d99f5d0759d61f846038a32a538f2888
etouch-solutions <etouchindiasolutions@gmail.com>
2025-07-31 14:03:16 +0530

    Add full project files

--- index.php
+++ index.php
@@ -8,16 +8,7 @@
 </head>
 <body>
   
-<a href="branch.php">add a branch</a> <br>
-<a href="add_staff.php">add a staff</a> <br>
-<a href="brand.php">add a brand</a> <br>
-<a href="add_defect.php">add a defect</a> <br>
-<a href="add_insurance.php">add an insurance type</a> <br>
-<a href="Customer_Master.php">add  a customer</a><br>
-<a href="Insurance_Entry.php">add a insurance for device</a> <br>
- <a href="claim_entry.php">add a claim for device</a> <br>
-
-    <a href="serch.php">add a claim for device</a>
+ 
 
 
 
