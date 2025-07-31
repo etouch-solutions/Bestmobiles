dc1e9ea23f9a37e9c2bf3c2d9e7bdde6590f8f8e
etouch-solutions <etouchindiasolutions@gmail.com>
2025-07-31 17:11:45 +0530

    Add full project files

--- brand.php
+++ brand.php
@@ -47,7 +47,7 @@ if (isset($_GET['edit'])) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>Brand Master</title>
-  <link rel="stylesheet" href="style.css" />
+  <link rel="stylesheet" href="styles.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
 <body>
