a35820727c083dde7be20e6e6f243b922bc11ce0
etouch-solutions <etouchindiasolutions@gmail.com>
2025-07-31 16:14:35 +0530

    Add full project files

--- branch.php
+++ branch.php
@@ -58,7 +58,21 @@ if (isset($_GET['edit'])) {
     <div class="hamburger" onclick="toggleSidebar()">☰</div>
   </div>
 
-  
+   <div class="container">
+    <aside class="sidebar mobile-hidden" id="sidebarMenu">
+      <ul>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="index.php"><li>Dashboard</li></a>
+     <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;" href="branch.php" class="active"> <li>Branch Master</li></a>
+     <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="brand.php" > <li>Brand Master</li></a>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="add_staff.php"><li>Staff Master</li></a>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="Customer_Master.php"><li>Customer Master</li></a>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="add_insurance.php"><li>Insurance Master</li></a>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="add_defect.php"><li>Defect Master</li></a>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="insurance_entry.php"><li>Insurance Entry</li></a>
+      <a style="text-decoration: none; color: #144d30ff; font-weight: 500; font-size: 14px;"  href="serch.php"><li>Claim</li></a>
+      </ul>
+    </aside>
+</div>
  <main class="main-content">
       <div class="content-area">
   <!-- Form Box -->
