b0d2ff42c79c6c937360d8b66a74ece29df9427e
etouch-solutions <etouchindiasolutions@gmail.com>
2025-08-06 11:34:56 +0530

    Add full project files

--- Insurance_Entry.php
+++ Insurance_Entry.php
@@ -21,136 +21,17 @@
 
  <aside class="sidebar mobile-hidden" id="sidebarMenu">
       <ul>
-        <li><a href="index.php">Dashboard</a></li>
-        <li><a href="branch.php">Branch Master</a></li>
-        <li><a href="brand.php">Brand Master</a></li>
-        <li><a href="add_staff.php">Staff Master</a></li>
-        <li><a href="Customer_Master.php">Customer Master</a></li>
-        <li><a href="add_insurance.php" class="active">Insurance Master</a></li>
-        <li><a href="add_defect.php">Defect Master</a></li>
-        <li><a href="insurance_entry.php">Insurance Entry</a></li>
-        <li><a href="serch.php">Claim</a></li>
+        <a href="index.php"><li>Dashboard</li></a>
+        <a href="branch.php" class="active"><li>Branch Master</li></a>
+        <a href="brand.php"><li>Brand Master</li></a>
+        <a href="add_staff.php"><li>Staff Master</li></a>
+        <a href="Customer_Master.php"><li>Customer Master</li></a>
+        <a href="add_insurance.php"><li>Insurance Master</li></a>
+        <a href="add_defect.php"><li>Defect Master</li></a>
+        <a href="insurance_entry.php"><li>Insurance Entry</li></a>
+        <a href="serch.php"><li>Claim</li></a>
       </ul>
     </aside>
-
-
-
-
-  <main class="main-content">
-    <div class="content-area">
-      <!-- Form Section -->
-      <section class="add-branch">
-        <h3>Add Insurance Entry</h3>
-        <form action="insert_insurance_entry.php" method="POST" enctype="multipart/form-data">
-          <label>Select Customer</label>
-          <select name="cus_id" onchange="loadCustomerDetails(this.value)" required>
-            <option value="">-- Select --</option>
-            <?php
-              $res = mysqli_query($conn, "SELECT Cus_Id, Cus_Name FROM Customer_Master WHERE Is_Active=1");
-              while ($row = mysqli_fetch_assoc($res))
-                echo "<option value='{$row['Cus_Id']}'>{$row['Cus_Name']}</option>";
-            ?>
-          </select>
-
-          <label>Select Brand</label>
-          <select name="brand_id" onchange="loadBrandDetails(this.value)" required>
-            <option value="">-- Select --</option>
-            <?php
-              $res = mysqli_query($conn, "SELECT Brand_Id, Brand_Name FROM Brands_Master WHERE Is_Active=1");
-              while ($row = mysqli_fetch_assoc($res))
-                echo "<option value='{$row['Brand_Id']}'>{$row['Brand_Name']}</option>";
-            ?>
-          </select>
-
-          <label>Select Insurance Plan</label>
-          <select name="insurance_id" id="insurance_id" onchange="calculatePremiumAndEndDate(); loadInsuranceDetails(this.value)" required>
-            <option value="">-- Select --</option>
-            <?php
-              $res = mysqli_query($conn, "SELECT Insurance_Id, Insurance_Name, Premium_Percentage, Duration_Months FROM Insurance_Master WHERE Is_Active=1");
-              while($r = mysqli_fetch_assoc($res))
-                echo "<option value='{$r['Insurance_Id']}' data-premium='{$r['Premium_Percentage']}' data-duration='{$r['Duration_Months']}'>{$r['Insurance_Name']}</option>";
-            ?>
-          </select>
-
-          <label>Select Staff</label>
-          <select name="staff_id" required>
-            <option value="">-- Select --</option>
-            <?php
-              $res = mysqli_query($conn, "SELECT Staff_Id, Staff_Name FROM Staff_Master WHERE Staff_Status=1");
-              while($r = mysqli_fetch_assoc($res))
-                echo "<option value='{$r['Staff_Id']}'>{$r['Staff_Name']}</option>";
-            ?>
-          </select>
-
-          <label>Product Model Name</label>
-          <input type="text" name="product_model_name" oninput="updatePreview('model', this)" required>
-
-          <label>IMEI 1</label>
-          <input type="text" name="imei_1" oninput="updatePreview('imei1', this)" required>
-
-          <label>IMEI 2</label>
-          <input type="text" name="imei_2" oninput="updatePreview('imei2', this)">
-
-          <label>Product Value (₹)</label>
-          <input type="number" name="product_value" id="product_value" oninput="calculatePremiumAndEndDate(); updatePreview('value', this)" required>
-
-          <label>Calculated Premium (₹)</label>
-          <input type="number" name="premium_amount" id="premium_amount" readonly>
-
-          <label>Upload Product Photo</label>
-          <input type="file" name="product_photo" accept="image/*" onchange="previewImage(this,'previewProductPhoto')">
-
-          <label>Upload Bill Photo</label>
-          <input type="file" name="bill_photo" accept="image/*" onchange="previewImage(this,'previewBillPhoto')">
-
-          <label>Bill Date</label>
-          <input type="date" name="bill_date" onchange="updatePreview('bill', this)" required>
-
-          <label>Insurance Start</label>
-          <input type="date" name="insurance_start" id="insurance_start" onchange="calculatePremiumAndEndDate(); updatePreview('start', this)" required>
-
-          <label>Insurance End</label>
-          <input type="date" name="insurance_end" id="insurance_end" readonly>
-
-          <label>Insurance Status</label>
-          <select name="insurance_status" onchange="updatePreview('insStatus', this)">
-            <option value="1">Valid</option>
-            <option value="0">Invalid</option>
-          </select>
-
-          <label>Product Insurance Status</label>
-          <select name="product_ins_status" onchange="updatePreview('prodStatus', this)">
-            <option value="1">Active</option>
-            <option value="0">Inactive</option>
-          </select>
-
-          <button type="submit">Submit</button>
-        </form>
-      </section>
-
-      <!-- Preview Panel -->
-      <section class="overview">
-        <h3>Live Preview</h3>
-        <div id="customerDetails">Select a customer to view details.</div>
-        <div id="brandDetails">Select a brand to view details.</div>
-        <div id="insuranceDetails">Select a plan to view details.</div>
-        <p id="previewModel">Model: -</p>
-        <p id="previewIMEI1">IMEI 1: -</p>
-        <p id="previewIMEI2">IMEI 2: -</p>
-        <p id="previewValue">Product Value: -</p>
-        <p id="previewPremium">Premium: -</p>
-        <p id="previewBillDate">Bill Date: -</p>
-        <p id="previewStart">Start Date: -</p>
-        <p id="previewEnd">End Date: -</p>
-        <p id="previewInsStatus">Insurance Status: -</p>
-        <p id="previewProdStatus">Product Insurance Status: -</p>
-        <div>Product Photo:<br><img id="previewProductPhoto" width="120"></div>
-        <div>Bill Photo:<br><img id="previewBillPhoto" width="120"></div>
-      </section>
-    </div>
-  </main>
-</div>
-
 <!-- JS -->
 <script>
 function toggleSidebar(){document.getElementById('sidebarMenu').classList.toggle('mobile-hidden');}
