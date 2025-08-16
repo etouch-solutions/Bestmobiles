319aaef9edaaa95dcfd8f1ad03efce24fc1a9834
etouch-solutions <etouchindiasolutions@gmail.com>
2025-08-16 11:13:16 +0530

    Add full project files

--- claim_entry.php
+++ claim_entry.php
@@ -1,57 +1,3 @@
-<?php
-include 'db.php';
-
-$insurance_id = isset($_GET['insurance_id']) ? (int)$_GET['insurance_id'] : 0;
-
-// Fetch insurance & customer info
-$info = null;
-if ($insurance_id > 0) {
-  $res = mysqli_query($conn, "
-    SELECT 
-      i.*, c.Cus_Name, c.Cus_CNo, c.Cus_Address,
-      ins.Insurance_Name
-    FROM Insurance_Entry i
-    JOIN Customer_Master c ON c.Cus_Id = i.Cus_Id
-    JOIN Insurance_Master ins ON ins.Insurance_Id = i.Insurance_Id
-    WHERE i.Insurance_Entry_Id = $insurance_id
-  ");
-  if (mysqli_num_rows($res)) {
-    $info = mysqli_fetch_assoc($res);
-  } else {
-    die("Insurance entry not found.");
-  }
-}
-
-// On submit
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $insurance_entry_id = (int)$_POST['insurance_entry_id'];
-  $defect_id = (int)$_POST['defect_id'];
-  $defect_value = (float)$_POST['defect_value']; // NEW FIELD
-  $remarks = mysqli_real_escape_string($conn, $_POST['remarks']);
-
-  // Upload image
-  $imgPath = '';
-  if (!empty($_FILES['claim_image']['name'])) {
-    $targetDir = "uploads/";
-    if (!is_dir($targetDir)) mkdir($targetDir, 0777, true);
-    $imgPath = $targetDir . time() . '_' . basename($_FILES['claim_image']['name']);
-    move_uploaded_file($_FILES['claim_image']['tmp_name'], $imgPath);
-  }
-
-  $query = "
-    INSERT INTO Claim_Entry (Insurance_Entry_Id, Defect_Id, Defect_Value, Claim_Remarks, Claim_Image_Path, Created_At)
-    VALUES ($insurance_entry_id, $defect_id, $defect_value, '$remarks', '$imgPath', NOW())
-  ";
-
-  if (mysqli_query($conn, $query)) {
-    echo "<script>alert('Claim Submitted Successfully'); location.href='insurance_history.php';</script>";
-    exit;
-  } else {
-    echo "<p style='color:red;'>Error: " . mysqli_error($conn) . "</p>";
-  }
-}
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -97,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         ?>
       </select> 
 
-      <!-- NEW FIELD FOR DEFECT VALUE -->
+      <!-- ✅ NEW FIELD FOR DEFECT VALUE -->
       <label>Defect Value (₹)</label>
       <input type="number" name="defect_value" min="0" step="0.01" required>
 
